af038a6a806e5b97c19fb57a454728570f45e58d
Le Ngoc Tuyen <lengoctuyen30032006@gmail.com>
2025-12-16 20:12:02 +0700

    fix bug

--- Client/obj/Debug/net7.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net7.0/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a8d7bf37f98ad20be716d84dbf44fe8238f8883")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
